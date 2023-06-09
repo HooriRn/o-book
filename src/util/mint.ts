ef9784e3ff35d4b31151295de6c2ac1c4f35f540
HooriRn <hoorirn@protonmail.com>
2023-06-10 00:51:09 +0330

    New update

---
+++ src/util/mint.ts
@@ -0,0 +1,52 @@
+// xchainjs
+import { Asset, assetAmount, assetFromString, assetToBase, assetToString, baseToAsset } from '@xchainjs/xchain-util';
+import { AssetRuneNative, Client as thorchainClient } from '@xchainjs/xchain-thorchain';
+import { CryptoAmount, ThorchainQuery } from '@xchainjs/xchain-thorchain-query';
+
+// dot env config
+import dotenv from 'dotenv'
+dotenv.config()
+
+import { BigNumber as bn } from 'bignumber.js'
+
+const query = new ThorchainQuery()
+const wallet = new thorchainClient({ phrase: process.env.PHRASE })
+
+async function doMint(fromAsset: Asset, toAsset: Asset, toAddress: string, value: number): Promise<string | undefined> {
+  const txDetails = await query.estimateSwap({
+    input: new CryptoAmount(assetToBase(assetAmount(value, 8)), fromAsset),
+    destinationAddress: toAddress,
+    destinationAsset: toAsset,
+    slipLimit: new bn("0.01")
+  })
+
+  console.log('See if it can be swapped', txDetails.txEstimate.canSwap) 
+  if (txDetails.txEstimate.canSwap) {
+    console.log('Swapping now with the output fee:', txDetails.txEstimate.totalFees.outboundFee.assetAmountFixedString())
+    console.log('Swapping now with the est output:', txDetails.txEstimate.netOutput.assetAmountFixedString())
+    const txID = await wallet.deposit({
+      asset: fromAsset,
+      amount: assetToBase(assetAmount(value, 8)),
+      memo: txDetails.memo,
+    })
+
+    return txID
+  }
+
+  return undefined
+}
+
+async function main() {
+  const balances = (await wallet.getBalance(wallet.getAddress())).map(a => ({asset: assetToString(a.asset), input: baseToAsset(a.amount).amount().toString()}))
+  const toMintAsset = assetFromString('BNB.BUSD-BD1') ?? undefined 
+
+  // do mint 100%
+  const toMintAmount = +balances.find(a => a.asset === 'BNB/BUSD-BD1').input * (1)
+
+  if (toMintAsset) {
+    const tx = await doMint(assetFromString('BNB/BUSD-BD1'), toMintAsset, 'bnb1c88g08m6w994ugzyz0hnfqnzs5c2vm27zqm2f9', toMintAmount)
+    console.log(tx)
+  }
+}
+
+main();
--- src/util/util.ts
+++ src/util/util.ts
@@ -1,4 +1,7 @@
 import { CryptoAmount, SwapEstimate, TxDetails } from "@xchainjs/xchain-thorchain-query"
+import { decryptFromKeystore, Keystore } from '@xchainjs/xchain-crypto'
+import * as fs from 'fs';
+const prompts = require('prompts');
 
 // Helper function for printing out the returned object
 function print(estimate: SwapEstimate, input: CryptoAmount) {
@@ -26,4 +29,21 @@ export function printTx(txDetails: TxDetails, input: CryptoAmount) {
     txEstimate: print(txDetails.txEstimate, input),
   }
   console.log(expanded)
+}
+
+//Read Keystore File
+export async function readKeystore(path: string) {
+  const keystore: Keystore = JSON.parse(await fs.readFileSync(path, 'utf-8'))
+
+  const questions = await prompts([
+    { 
+      type: 'password', 
+      name: 'password', 
+      message: 'Type your password'
+    }
+  ]);
+
+  const phrase = await decryptFromKeystore(keystore, questions.password)
+
+  return phrase
 }
